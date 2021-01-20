c4de8aeaaa995f5af625b024f37cd95f5a5c5c5d
faculdadesensu <faculdadesensu@gmail.com>
2021-01-20 12:58:36 -0300

    Implementações do front end. 20/01/2021 - 12:58

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -9,72 +10,52 @@
 
     <title>Midias Faculdade Sensu</title>
 
-    <!-- Custom fonts for this template-->
-    <link href="{{ URL::asset('vendor/fontawesome-free/css/all.min.css')}}" rel="stylesheet" type="text/css">
-    <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
-    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300&display=swap" rel="stylesheet">
+    <link href="{{ URL::asset('css/style_index.css')}}" rel="stylesheet">
 
-    <!-- Custom styles for this template-->
-    <link href="{{ URL::asset('css/sb-admin-2.min.css')}}" rel="stylesheet">
-    <link href="{{ URL::asset('css/style.css')}}" rel="stylesheet">
-    
-    <link href="{{ URL::asset('vendor/datatables/dataTables.bootstrap4.min.css')}}" rel="stylesheet">
-    <!-- Bootstrap core JavaScript-->
-    <script src="{{ URL::asset('vendor/jquery/jquery.min.js')}}"></script>
-    <script src="{{ URL::asset('vendor/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
-    
     <link rel="shortcut icon" href="{{ URL::asset('img/favicon.ico')}}" type="image/x-icon">
     <link rel="icon" href="{{ URL::asset('img/favicon.ico')}}" type="image/x-icon">
 
+    <!-- Custom fonts for this template-->
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300&display=swap" rel="stylesheet">
 </head>
+
 <body>
-    <div class="col-total">     
+    <div class="col-total">
         <div class="col-botton">
-            <img class="img-logo" src="{{ URL::asset('img/Asset 9@3x.png')}}" >
+            <img class="img-logo" src="{{ URL::asset('img/Asset 9@3x.png')}}">
             <?php
-              use App\Models\Link;
-              $links = Link::orderby('id', 'desc')->paginate();  
+
+            use App\Models\Link;
+
+            $links = Link::orderby('id', 'desc')->paginate();
+            // Variável contador para verificar quando é a ultima linha.
+            $count = 0;
             ?>
             @foreach ($links as $item)
-            <p><a class="btn2" href="{{$item->link}}">{{$item->title}}</a></p>
-            <hr >
+            <?php $count++; ?>
+            @if($links->count() != $count)
+            <p><a class="btn" href="{{$item->link}}">{{$item->title}}</a></p>
+            <hr size=1px>
+            @else
+            <p><a id="btn-botton" class="btn" href="{{$item->link}}">{{$item->title}}</a></p>
+            @endif
             @endforeach
             <div class="icon">
-                <a class= "icon-social" id="whatsapp" href="https://api.whatsapp.com/send?phone=556239334450" >.....</a>
-                <a class= "icon-social" id="instagram" href="https://www.instagram.com/faculdade_sensu/">.....</a>
-                <a class= "icon-social" id="facebook" href="https://www.facebook.com/faculdadesensu">.....</a>
-                <a class= "icon-social" id="youtube" href="https://www.youtube.com/channel/UCS2qho79tP4hznASgyOFb0A?view_as=subscriber" >.....</a>
-                <a class= "icon-social" id="linkdin" href="https://br.linkedin.com/company/faculdadesensu" >.....</a>
-                <a class= "icon-social" id="twitter" href="https://twitter.com/FaculdadeSensu" >.....</a>
+                <a class="icon-social" id="whatsapp" href="https://api.whatsapp.com/send?phone=556239334450">.....</a>
+                <a class="icon-social" id="instagram" href="https://www.instagram.com/faculdade_sensu/">.....</a>
+                <a class="icon-social" id="facebook" href="https://www.facebook.com/faculdadesensu">.....</a>
+                <a class="icon-social" id="youtube" href="https://www.youtube.com/channel/UCS2qho79tP4hznASgyOFb0A?view_as=subscriber">.....</a>
+                <a class="icon-social" id="linkdin" href="https://br.linkedin.com/company/faculdadesensu">.....</a>
+                <a class="icon-social" id="twitter" href="https://twitter.com/FaculdadeSensu">.....</a>
             </div>
-           </div>
-   </div>
-    <footer class="footer122">        
-         <div class="footer2">
-        		<small class="d-block mb-3 text-muted">&copy; Powered by: FAS - 2020</small>
-		</div>
-	</footer>  
+        </div>
+    </div>
+    <footer class="footer1">
+        <div class="footer">
+            <small class="d-block mb-3 text-muted">&copy; Powered by: FAS - 2020</small>
+        </div>
+    </footer>
 </body>
-        <!--<script type="text/javascript" async src="https://d335luupugsy2.cloudfront.net/js/loader-scripts/d2db8af8-3ac5-4b11-962e-abf1a30aa282-loader.js" ></script>-->
-        <!-- Core plugin JavaScript-->
-        <script src="{{ URL::asset('vendor/jquery-easing/jquery.easing.min.js')}}"></script>
+<script type="text/javascript" async src="https://d335luupugsy2.cloudfront.net/js/loader-scripts/d2db8af8-3ac5-4b11-962e-abf1a30aa282-loader.js"></script>
 
-        <!-- Custom scripts for all pages-->
-        <script src="{{ URL::asset('js/sb-admin-2.min.js')}}"></script>
-
-        <!-- Page level plugins -->
-        <script src="{{ URL::asset('vendor/chart.js/Chart.min.js')}}"></script>
-
-        <!-- Page level custom scripts -->
-        <script src="{{ URL::asset('js/demo/chart-area-demo.js')}}"></script>
-        <script src="{{ URL::asset('js/demo/chart-pie-demo.js')}}"></script>
-
-        <!-- Page level plugins -->
-        <script src="{{ URL::asset('vendor/datatables/jquery.dataTables.min.js')}}"></script>
-        <script src="{{ URL::asset('vendor/datatables/dataTables.bootstrap4.min.js')}}"></script>
-
-        <!-- Page level custom scripts -->
-        <script src="{{ URL::asset('js/demo/datatables-demo.js')}}"></script>
-        <script src="{{ URL::asset('js/mascaras.js')}}"></script>
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.mask/1.14.11/jquery.mask.min.js"></script>
 </html>
--- resources/views/login/index.blade.php
+++ resources/views/login/index.blade.php
@@ -1,59 +1,33 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Login Administrador</title>
 
-    <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
-    <script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
-    <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-    <!------ Include the above in your HEAD tag ---------->
+    <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.bundle.min.js"></script>
 
-    <link href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
-    <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/js/bootstrap.min.js"></script>
-    <script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
-    <!-- Include the above in your HEAD tag -->
-
-    <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
     <style>
-        @charset "utf-8";
-        @import url//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css);
-
-        div.main{
-            background: #0264d6; /* Old browsers */
-            background: -moz-radial-gradient(center, ellipse cover,  #0264d6 1%, #1c2b5a 100%); /* FF3.6+ */
-            background: -webkit-gradient(radial, center center, 0px, center center, 100%, color-stop(1%,#0264d6), color-stop(100%,#1c2b5a)); /* Chrome,Safari4+ */
-            background: -webkit-radial-gradient(center, ellipse cover,  #0264d6 1%,#1c2b5a 100%); /* Chrome10+,Safari5.1+ */
-            background: -o-radial-gradient(center, ellipse cover,  #0264d6 1%,#1c2b5a 100%); /* Opera 12+ */
-            background: -ms-radial-gradient(center, ellipse cover,  #0264d6 1%,#1c2b5a 100%); /* IE10+ */
-            background: radial-gradient(ellipse at center,  #0264d6 1%,#1c2b5a 100%); /* W3C */
-            filter: progid:DXImageTransform.Microsoft.gradient( startColorstr='#0264d6', endColorstr='#1c2b5a',GradientType=1 ); /* IE6-9 fallback on horizontal gradient */
-            height:calc(100vh);
-            width:100%;
-        }
-
-        [class*="fontawesome-"]:before {
-        font-family: 'FontAwesome', sans-serif;
+        body {
+            background: radial-gradient(ellipse at center, #4a8dba 1%, #125591 70%);
+            height: calc(100vh);
+            width: 100%;
+            color: #606468;
+            font: 87.5%/1.5em 'Open Sans', sans-serif;
+            margin: 0;
         }
 
         /* ---------- GENERAL ---------- */
 
         * {
-        box-sizing: border-box;
-            margin:0px auto;
-
-            &:before,
-            &:after {
-                box-sizing: border-box;
-            }
-        }
-
-        body {
-        color: #606468;
-        font: 87.5%/1.5em 'Open Sans', sans-serif;
-        margin: 0;
+            box-sizing: border-box;
+            margin: 0px auto;
         }
 
         a {
@@ -79,45 +53,38 @@
         }
 
         .clearfix {
-        *zoom: 1;
-
-        &:before,
-        &:after {
-            content: ' ';
-            display: table;
-        }
-
-        &:after {
-            clear: both;
-        }
-
+            *zoom: 1;
         }
 
         .container {
-        left: 50%;
-        position: fixed;
-        top: 50%;
-        transform: translate(-50%, -50%);
+            left: 50%;
+            position: fixed;
+            top: 50%;
+            transform: translate(-50%, -50%);
         }
 
         /* ---------- LOGIN ---------- */
 
-        #login form{
+        #login form {
             width: 250px;
         }
-        #login, .logo{
-            display:inline-block;
-            width:40%;
+
+        #login,
+        .logo {
+            display: inline-block;
+            width: 40%;
         }
-        #login{
-        border-right:1px solid #fff;
-        padding: 0px 22px;
-        width: 59%;
+
+        #login {
+            border-right: 1px solid #fff;
+            padding: 0px 22px;
+            width: 59%;
         }
-        .logo{
-        color:#fff;
-        font-size:50px;
-        line-height: 125px;
+
+        .logo {
+            color: #fff;
+            font-size: 50px;
+            line-height: 125px;
         }
 
         #login form span.fa {
@@ -127,7 +94,7 @@
             display: block;
             float: left;
             height: 50px;
-            font-size:24px;
+            font-size: 24px;
             line-height: 50px;
             text-align: center;
             width: 50px;
@@ -136,13 +103,15 @@
         #login form input {
             height: 50px;
         }
-        fieldset{
-            padding:0;
-            border:0;
-            margin: 0;
 
+        fieldset {
+            padding: 0;
+            border: 0;
+            margin: 0;
         }
-        #login form input[type="text"], input[type="password"] {
+
+        #login form input[type="text"],
+        input[type="password"] {
             background-color: #fff;
             border-radius: 0px 3px 3px 0px;
             color: #000;
@@ -152,53 +121,55 @@
         }
 
         #login form input[type="submit"] {
-        border-radius: 3px;
-        -moz-border-radius: 3px;
-        -webkit-border-radius: 3px;
-        background-color: #000000;
-        color: #eee;
-        font-weight: bold;
-        /* margin-bottom: 2em; */
-        text-transform: uppercase;
-        padding: 5px 10px;
-        height: 30px;
+            border-radius: 3px;
+            -moz-border-radius: 3px;
+            -webkit-border-radius: 3px;
+            background-color: #000000;
+            color: #eee;
+            font-weight: bold;
+            /* margin-bottom: 2em; */
+            text-transform: uppercase;
+            padding: 5px 10px;
+            height: 30px;
         }
 
         #login form input[type="submit"]:hover {
             background-color: #d44179;
         }
 
-        #login > p {
+        #login>p {
             text-align: center;
         }
 
-        #login > p span {
+        #login>p span {
             padding-left: 5px;
         }
+
         .middle {
-        display: flex;
-        width: 600px;
+            display: flex;
+            width: 600px;
         }
     </style>
 </head>
+
 <body>
     <div class="main">
         <div class="container">
             <center>
-            <div class="middle">
-                <div id="login">
-                    <form action="{{route('login')}}" method="post">
-                        @csrf
-                        <fieldset class="clearfix">
-                            <p ><span class="fa fa-user"></span><input name="username" type="text"  Placeholder="Usuário" required></p>
-                            <p><span class="fa fa-lock"></span><input name="password" type="password"  Placeholder="Senha" required></p> 
-                            <div>
-                                <span style="width:100%; text-align:right;  display: inline-block;"><input type="submit" value="Entrar"></span>
-                            </div>
-                        </fieldset>
+                <div class="middle">
+                    <div id="login">
+                        <form action="{{route('login')}}" method="post">
+                            @csrf
+                            <fieldset class="clearfix">
+                                <p><span class="fa fa-user"></span><input name="username" type="text" Placeholder="Usuário" required></p>
+                                <p><span class="fa fa-lock"></span><input name="password" type="password" Placeholder="Senha" required></p>
+                                <div>
+                                    <span style="width:100%; text-align:right;  display: inline-block;"><input type="submit" value="Entrar"></span>
+                                </div>
+                            </fieldset>
+                            <div class="clearfix"></div>
+                        </form>
                         <div class="clearfix"></div>
-                    </form>
-                    <div class="clearfix"></div>
                     </div> <!-- end login -->
                     <div class="logo">LOGO
                         <div class="clearfix"></div>
@@ -208,4 +179,5 @@
         </div>
     </div>
 </body>
+
 </html>
