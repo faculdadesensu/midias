a8f1ccf77149b443181528429bc72fa9b722a6c9
faculdadesensu <faculdadesensu@gmail.com>
2021-01-22 14:49:38 -0300

    finalização retorno banco

--- app/Http/Controllers/MoodleController.php
+++ app/Http/Controllers/MoodleController.php
@@ -2,6 +2,11 @@
 
 namespace App\Http\Controllers;
 
+use DateTime;
+use DateTimeZone;
+use Exception;
+use Illuminate\Support\Facades\DB;
+
 class MoodleController extends Controller
 {
     public function index()
@@ -12,7 +17,7 @@ class MoodleController extends Controller
     public function service($id_suspended, $userid)
     {
         try {
-            /*$userid         = $userid;
+            $userid         = $userid;
             $id_suspended   = $id_suspended;
             $result         = DB::connection('mysql2')->select('select userid from mdl_role_assignments where roleid in (2, 3, 4)');
 
@@ -35,7 +40,8 @@ class MoodleController extends Controller
                 $institution    = $user[0]->institution;
 
                 $this->reports($userid, $username, $email, $institution, $firstname, $lastname);
-            }*/
+            }
+            
             return redirect()->route('moodle.index')->with('success', utf8_encode('Operação realizada com sucesso.'));
 
         } catch (\Throwable $th) {
@@ -45,11 +51,67 @@ class MoodleController extends Controller
 
     public function lock()
     {
-        $this->service(1, 1);
+        try {
+            $this->service(1, 1);
+            return redirect()->route('moodle.index')->with('success', utf8_encode('Operação realizada com sucesso.'));
+        } catch (\Throwable $th) {
+            return redirect()->route('moodle.index')->with('error', utf8_encode('Erro desconhecido!'));
+        }
     }
 
     public function unlock()
     {
-        $this->service(0, 0);
+        try {
+            $this->service(0, 0);
+            return redirect()->route('moodle.index')->with('success', utf8_encode('Operação realizada com sucesso.'));
+        } catch (\Throwable $th) {
+            return redirect()->route('moodle.index')->with('error', utf8_encode('Erro desconhecido!'));
+        }
+    }
+
+    public function reports($userid, $username, $email, $institution, $firstname, $lastname)
+    {
+        try {
+            $userid         = $userid;
+            $username       = $username;
+            $email          = $email;
+            $institution    = $institution;
+            $firstname      = $firstname;
+            $lastname       = $lastname;
+
+            $timezone   = new DateTimeZone('America/Sao_Paulo');
+            $time       = new DateTime('now', $timezone);
+            $time2      = date_format($time, 'Y/m/d H:i:s');
+
+            DB::insert('insert into reports (userid,username, time, email, institution, firstname, lastname) values ( ?,?,?,?,?,?,?)', [
+                $userid,
+                $username,
+                $time2,
+                $email,
+                $institution,
+                $firstname,
+                $lastname
+
+            ]);
+        } catch (\Throwable $th) {
+            throw new Exception("Falha ao realizar a operação!");
+        }
+    }
+
+    public function viewReports()
+    {
+        try {
+            $results = DB::table('reports')->get();
+
+            foreach ($results as $result) {
+                $time = new DateTime($result->time);
+                $time2 = date_format($time, 'd/m/Y H:i:s');
+                $result->time = $time2;
+            }
+
+            return view('painel-admin.moodle.index', ['results' => $results]);
+        } catch (\Throwable $th) {
+            return view('erro');
+        }
     }
 }
