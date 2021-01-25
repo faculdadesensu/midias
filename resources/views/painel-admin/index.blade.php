56d7a27b579b275bc92ba760462fccbdf7dee8b7
faculdadesensu <faculdadesensu@gmail.com>
2021-01-25 15:53:46 -0300

    Criando recurso de apresentação de cards com os links cadastrados

--- resources/views/painel-admin/index.blade.php
+++ resources/views/painel-admin/index.blade.php
@@ -7,10 +7,23 @@
 if(@$_SESSION['level'] != 'admin' && @$_SESSION['level'] != 'user' ){ 
   echo "<script language='javascript'> window.location='./' </script>";
 }
-
+use App\Models\Link;
+$links = link::orderby('id', 'desc')->get();
 ?>
-<div class="card col-lg-4 col-md-8 col-sm-12 embed-responsive embed-responsive-1by1">
-  <iframe src="{{url('https://conteudo.faculdadesensu.edu.br/9-dicas-para-o-enem')}}">Your browser isn't compatible</iframe>
+<h3 class="mb-5"><i>MONITORAMENTO DE LINKS</i></h3>
+<div class="row ml-2">
+  @foreach ($links as $item)
+  <div class="mr-5 mb-5" style="box-shadow: 0 0 1em rgb(0,0,0, 0.2); border-radius: 10px">
+    <div class="card" style="width:300px">
+      <div class="embed-responsive embed-responsive-1by1">
+        <iframe src="{{$item->link}}"></iframe>
+      </div>
+      <div class="card-body mr-2">
+        <h4 class="card-title">{{$item->title}}</h4>
+        <a href="{{$item->link}}" class="btn btn-primary" target="_blank">Acessar Link</a>
+      </div>
+    </div>
+  </div>
+  @endforeach
 </div>
-
 @endsection
