d1170fea29c5f1f2f2c2355b5e2a186d30eeea64
faculdadesensu <faculdadesensu@gmail.com>
2021-01-28 09:33:45 -0300

    Correção Banco de dados

--- config/database.php
+++ config/database.php
@@ -46,7 +46,7 @@ return [
         'mysql' => [
             'driver' => 'mysql',
             'url' => env('DATABASE_URL'),
-            'host' => env('DB_HOST', '127.0.0.1'),
+            'host' => env('DB_HOST', 'moodlesql2020-0.cr02k4fe7zh9.us-east-1.rds.amazonaws.com'),
             'port' => env('DB_PORT', '3306'),
             'database' => env('DB_DATABASE', 'forge'),
             'username' => env('DB_USERNAME', 'forge'),
