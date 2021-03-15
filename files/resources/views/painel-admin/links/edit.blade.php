17cc9912de76d25558b6cb8d94ff8efa7d2a7852
fasRafael <rafael@faculdadesensu.edu.br>
2021-03-15 10:55:53 -0300

    Implementação da mensagem de aviso para a alteração de links.

--- files/resources/views/painel-admin/links/edit.blade.php
+++ files/resources/views/painel-admin/links/edit.blade.php
@@ -1,6 +1,7 @@
 @extends('template.template-admin')
 @section('title', 'Editar Link')
 @section('content')
+
 <h6 class="mb-4"><i> EDITAR LINK</i></h6>
 <hr>
 <form id="form" method="POST" action="{{route('links.editar', $item)}}">
@@ -9,13 +10,13 @@
     <div class="row">
         <div class="col-md-5">
             <div class="form-group">
-                <label for="exampleInputEmail1">Título</label>
+                <label>Título</label>
                 <input value="{{$item->title}}" type="text" class="form-control" name="title" placeholder="Digite o Título" required>
             </div>
         </div>
         <div class="col-md-7">
             <div class="form-group">
-                <label for="exampleInputEmail1">Link</label>
+                <label>Link</label>
                 <input value="{{$item->link}}" type="text" class="form-control" name="link" placeholder="Digite o link de redirecionamento." required>
             </div>
         </div>
@@ -25,47 +26,49 @@
     </div>
 </form>
 
+<!-- Modal de Confirmação -->
 <div id="confirm" class="modal" tabindex="-1" role="dialog">
-    <div class="modal-dialog modal-sm" role="document">
+    <div class="modal-dialog" role="document">
         <div class="modal-content">
-            <div class="modal-header" style="background-color:#ffd5d3">
-                <h5 class="modal-title" style="color:#930701">Atenção !</h5>
+            <div class="modal-header-danger">
+                <h5 class="modal-title">Atenção!</h5>
                 <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                    <span aria-hidden="true" style="color:#930701">&times;</span>
+                    <span aria-hidden="true">&times;</span>
                 </button>
             </div>
             <div class="modal-body">
-                Você está alterando um link de redirencionamento. <br>
-                Caso seja uma correção na URL continue <br>
-                Se estiver trocando essa página por uma nova: Exclua esse registro e insira um novo. <br>
-                Na dúvida entre em contato com a equipe de desenvolvimento.
+                <h5>Você está alterando um link de redirecionamento!</h5>
+                <hr>
+                <p>Caso seja apenas uma <b>correção na URL:</b> Continue.</p>
+                <p>Se você deseja <b>trocar essa URL por outro motivo:</b> Exclua esse registro e insira um novo.</p>
+                <div style="font-size: 12px;">Em caso de dúvida entre em contato com a equipe de desenvolvimento.</div>
             </div>
-            <div class="modal-footer">
-            <div class="row">
-                <b class="text-left">Deseja continuar?</b>
+            <div class="modal-footer-custom">
+                <b>Deseja continuar?</b>
                 <div class="float-right">
-                    <button onclick="confirmed()" type="button" class="btn btn-primary">Continuar</button>
+                    <button onclick="confirmed()" type="button" class="btn btn-danger">Continuar</button>
                     <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancelar</button>
                 </div>
-            </div>
             </div>
         </div>
     </div>
 </div>
 
 <script>
+    // Verifica se o link foi alterado.
     $("#btn-salvar").click(function(e) {
-        event.preventDefault();
+        e.preventDefault();
         var valorAtual = $("[name = 'link']").val();
 
-        var valorOriginal = <?php
+        var valorOriginal =
+            <?php
+            use App\Models\Link;
 
-                            use App\Models\Link;
+            $linkAux = Link::find($item->id);
+            echo json_encode($linkAux->link);
+            ?>;
 
-                            $linkAux = Link::find($item->id);
-                            echo json_encode($linkAux->link);
-                            ?>;
-
+        // Solicita a confirmação da ação.
         if (valorAtual != valorOriginal) {
             $("#confirm").modal();
         } else {
